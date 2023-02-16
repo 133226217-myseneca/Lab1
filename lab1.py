44ceeebd6935294e5d9825fcbc370a3f4acb843d
133226217-myseneca <sislam101@myseneca.ca>
2023-02-15 19:10:25 -0500

    Added helloWorld() function to lab1.py

---
+++ lab1.py
@@ -0,0 +1,13 @@
+def calculate_age():
+    birth_year = input("Enter your birth year: ")
+    current_year = input("Enter the current year: ")
+    age = int(current_year) - int(birth_year)
+    print("Your age is:", age)
+
+calculate_age()
+
+def helloWorld():
+    print('Hello World')
+
+helloWorld()
+
